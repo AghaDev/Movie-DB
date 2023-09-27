b4dce8af98b1ffbfe4e8273fc972d0de8bfb83b9
AghaDev <mohamadagha2@gmail.com>
2023-09-28 02:13:00 +0300

    step3

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 app.get("/", (request, response) => {
     response.send("ok");
 });
 
-app.listen(3000, () => {
-    console.log("port 3000 listening");
+app.get("/test", (request, response) => {
+    response.send(" {status:200, message:'ok'}");
+});
+
+var time = new Date().getHours()  + ":" + new Date().getMinutes()
+
+console.log(time)
+app.get("/time", (request, response) => {
+    response.send(" {status:200, message:"+ time+ "}");
 });
 
